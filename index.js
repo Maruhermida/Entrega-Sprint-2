a09c98726071cb344065cf21da34894b96df6578
Maruhermida <maruhermida08@gmail.com>
2022-06-04 18:19:05 -0300

    funciones java

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 //listas de nombres y montos
 let listaNombres = []
 let listaMontos =[]
+let listaFinal = []
 
 //función cargar elementos a las listas
 
 function cargar(nom, mon) {
     listaNombres.push(nom);
     listaMontos.push(mon);
+    listaFinal.push(nom + mon)
 }
 
 
@@ -20,7 +22,21 @@ function capturar() {
 }
 
 
+//funcion calculo listaMontos
+function calcularTotal(listaMontos) {
+    var total = 0
+    for (let monto of listaMontos) {
+        total += monto
+    }
+}
+
+// función para el cálculo del monto total
+ function Total() {
+     const montoTotal = document.getElementById(total)
+     resultado.innertext = calcularTotal(listaMontos)
+ }
 
 
 
+ 
 
