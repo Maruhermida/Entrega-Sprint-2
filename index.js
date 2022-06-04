a488835bddfe46381190f1e8ed9db406c3663bf7
Maruhermida <maruhermida08@gmail.com>
2022-06-04 17:49:38 -0300

    js funciones

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const nombre = document.getElementById("nombreInput");
-const monto = document.getElementById("montoInput")
 
+//listas de nombres y montos
 let listaNombres = []
 let listaMontos =[]
 
+//función cargar elementos a las listas
 
+function cargar(nom, mon) {
+    listaNombres.push(nom);
+    listaMontos.push(mon);
+}
+
+
+// función para capturar datos del formulario 
+function capturar() {
+    const nombre = document.getElementById("nombreInput").value;
+    const monto = document.getElementById("montoInput").value;
+    cargar(nombre, monto);
+
+}
 
 
 
