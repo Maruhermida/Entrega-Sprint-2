8a294e12e65f173221c74676a47833de9684d953
Maruhermida <maruhermida08@gmail.com>
2022-06-27 09:17:25 -0300

    funcion captura

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let Personas = [];
 let Montos = [];
 
- 
 
+  
+function capturar() {
+  let Nombre = document.getElementById(nombre);
+  let Cantidad = document.getElementById(monto);
+    console.log(Nombre + toString(Cantidad));
+}
