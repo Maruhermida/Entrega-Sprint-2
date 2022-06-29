70d13d178e5eeb5f62928d37e8e4446790c0640a
Maruhermida <maruhermida08@gmail.com>
2022-06-29 17:11:59 -0300

    casi listo

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 let Personas = [];
 let Montos = [];
+let GastoT= 0;
 
+function total() {
+  GastoT += monto;
+}
 
-  
+function cargar(nom, mon) {
+  Personas.push(nom);
+  Montos.push(mon);
+}
+
 function capturar() {
-  let Nombre = document.getElementById("nombre").value;
-  let Cantidad = document.getElementById("monto").value;
-  console.log(Nombre + toString(Cantidad))
-    }
+  const nombre = document.getElementById("nombre").value;
+  const monto = document.getElementById("monto").value;
+  cargar(nombre, monto);
+}
+
+function mostrar(nom, mon) {
+  const lista = Montos
+  let p = document.createElement("p");
+  p.innerText = nom + " " + mon;
+  lista.append(p);
+}
+
+function promedio(mon) {
+  let numper = Personas.length;
+  let prom = (mon/numper).toFixed(2);
+  total.innerHTML = mon
+  final.innerHTML = prom
+}
+
+const persona = document.getElementById("nombre").value;
+const gasto = document.getElementById("monto").value;
+
+function acomodar() {
+  let nom = persona;
+  let mon = toString(gasto);
+  total();
+  capturar();
+  mostrar(nom, mon);
+  promedio(mon);
+}
