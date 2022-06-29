7c517bf94b8329fd95cee7c3dfa7ed72a321690f
Maruhermida <maruhermida08@gmail.com>
2022-06-29 16:04:14 -0300

    avances

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let Montos = [];
 
   
 function capturar() {
-  let Nombre = document.getElementById(nombre);
-  let Cantidad = document.getElementById(monto);
-    console.log(Nombre + toString(Cantidad));
-}
+  let Nombre = document.getElementById("nombre").value;
+  let Cantidad = document.getElementById("monto").value;
+    console.log(Nombre + Cantidad.toString)
+    }
