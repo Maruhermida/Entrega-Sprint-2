2971c7e2a8cd59d475a28c3d55bfe4eefd12bc42
Maruhermida <maruhermida08@gmail.com>
2022-07-02 19:25:00 -0300

    arreglo final lista de gastos

--- index.js
+++ index.js
@@ -10,22 +10,21 @@ const listagastos = document.getElementById('listagastos')
 function cargar() {
   listaPersonas.push(nombre);
   listaMontos.push(parseInt(monto));
-  return persona = {nombre: nombre, monto: monto}
 }
 
 function capturar() {  
   cargar(nombre, parseInt(monto.value));
-  console.log(parseInt(monto.value));
-  
+  console.log(parseInt(monto.value));  
 }
 
 function total() {
   gastoTotal += parseInt(monto.value);
+  return gastoTotal
 }
 
 function mostrar(nombre, monto) {
   let p = document.createElement('p');
-  p.textContent = nombre + "debe" + monto;
+  p.textContent = nombre + " debe " + monto;
   listagastos.append(p);
 }
 
@@ -39,8 +38,10 @@ function promedio() {
 
 
 function acomodar() {
+  const nombre = document.getElementById('nombre').value;
+  const monto = document.getElementById('monto').value;
   capturar();
   total(); 
-  mostrar(persona);
+  mostrar(nombre, monto);
   promedio();
 }
